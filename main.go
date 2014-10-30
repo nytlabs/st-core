1ecfbf747eb5d0a4b9159c40a36cfec0970ac399
Mike Dewar <mikedewar@gmail.com>
2014-10-30 10:26:59 -0400

    in the example main.go the pusher is removed from the pattern early to see what happens

--- main.go
+++ main.go
@@ -14,17 +14,21 @@ func main() {
 	d := core.NewDelay()
 	l := core.NewLog("logger")
 
-	supervisor.Add(p)
-	supervisor.Add(d)
-	supervisor.Add(l)
+	ptoken := supervisor.Add(p)
+	_ = supervisor.Add(d)
+	_ = supervisor.Add(l)
 
 	p.Connect("out", d.GetInput("in"))
 	d.Connect("out", l.GetInput("in"))
 
-	timer := time.NewTimer(500 * time.Millisecond)
+	timer1 := time.NewTimer(500 * time.Millisecond)
+	timer2 := time.NewTimer(2 * time.Second)
 
 	go supervisor.ServeBackground()
 
-	<-timer.C
+	<-timer1.C
+	supervisor.Remove(ptoken)
+
+	<-timer2.C
 	supervisor.Stop()
 }

402bff916d00ba4717d9164affa15e9b5037d43b
nik <nikhan@gmail.com>
2014-12-09 17:52:37 -0500

    continued sketching

--- thinking/simple/main.go
+++ thinking/simple/main.go
@@ -8,6 +8,14 @@ import (
 	"github.com/thejerf/suture"
 )
 
+const (
+	QUIT = iota
+	ADD_CONNECTION
+	DEL_CONNECTION
+	SET_PATH
+	SET_VALUE
+)
+
 type RouteID int64
 type Message interface{}
 
@@ -18,8 +26,11 @@ type Route struct {
 	C     chan Message
 }
 
-func (r *Route) SetValue(m Message) {
-	r.Value = m
+type Connection chan Message
+
+type Output struct {
+	Name        string
+	Connections map[Connection]struct{}
 }
 
 type MessageMap map[RouteID]Message
@@ -28,41 +39,75 @@ type RxKernel func(MessageMap, MessageMap, chan struct{}) bool
 
 type Block struct {
 	Inputs  []Route
-	Outputs []Route
+	Outputs []Output
 	Kernel  RxKernel
+	Control chan interface{}
 	Quit    chan struct{}
 }
 
 func NewBlock() *Block {
 	return &Block{
 		Inputs:  []Route{},
-		Outputs: []Route{},
+		Outputs: []Output{},
+		Control: make(chan interface{}),
 		Quit:    make(chan struct{}),
 	}
 }
 
 func (b *Block) Serve() {
-	inValues := make(MessageMap)
-	outValues := make(MessageMap)
-
+	inputs := make(MessageMap)
+	outputs := make(MessageMap)
 	for {
-		if ok := b.Receive(inValues); !ok {
+		if ok := b.Receive(inputs); !ok {
 			break
 		}
 
-		if ok = b.Kernel(inValues, outValues, b.QuitChan); !ok {
+		if ok = b.Kernel(inputs, outputs, b.QuitChan); !ok {
 			break
 		}
 
-		if ok = b.Broadcast(outValues); !ok {
+		if ok = b.Broadcast(outputs); !ok {
 			break
+		}
+
+		// we've successfully completed one full loop
+		// empty input buffer
+		for k, _ := range inputs {
+			delete(inputs, k)
 		}
 	}
+}
+
+func (b *Block) SetValue(id RouteId, value Message) {
+	b.Break <- struct{}{}
+}
+
+func (b *Block) SetPath(id RouteId, value *fetch.Query) {
+	b.Break <- struct{}{}
+}
+
+func (b *Block) Connect(id RouteId, c Connection) {
+	b.Break <- struct{}{}
+}
+
+func (b *Block) Disconnect(id RouteId, c Connection) {
+	b.Break <- struct{}{}
+}
+
+func (b *Block) Stop() {
+	b.Break <- struct{}{}
 }
 
 func (b *Block) Receive(values MessageMap) bool {
 	var err Error
 	for id, input := range b.Inputs {
+		//if we have already received a value on this input, skip.
+		if _, ok := values[id]; ok {
+			continue
+		}
+
+		// if there is a value set for this input, place value on
+		// buffer and set it in map.
 		if input.Value != nil {
 			values[id] = *input.Value
 			continue
@@ -70,10 +115,11 @@ func (b *Block) Receive(values MessageMap) bool {
 
 		select {
 		case m := <-input.C:
-			values[id], err = fetch.Run(input.Path, m)
+			v, err = fetch.Run(input.Path, m)
 			if err != nil {
 				log.Fatal(err)
 			}
+			values[id] = v
 		case <-b.QuitChan:
 			return nil, false
 		}
@@ -93,10 +139,6 @@ func (b *Block) Broadcast(values MessageMap) bool {
 		}*/
 	}
 	return true
-}
-
-func (b *Block) Stop() {
-	b.Quit <- struct{}{}
 }
 
 func main() {

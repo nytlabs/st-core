f8a834debbe4419300fffd3fb48cae4bc0e617fd
nik <nikhan@gmail.com>
2014-12-09 23:40:48 -0500

    interrupts

--- thinking/simple/main.go
+++ thinking/simple/main.go
@@ -1,22 +1,15 @@
 package main
 
 import (
-	"log"
-	"time"
-
+	"fmt"
 	"github.com/nikhan/go-fetch"
 	"github.com/thejerf/suture"
+	"log"
+	"math/rand"
+	"time"
 )
 
-const (
-	QUIT = iota
-	ADD_CONNECTION
-	DEL_CONNECTION
-	SET_PATH
-	SET_VALUE
-)
-
-type RouteID int64
+type RouteID int
 type Message interface{}
 
 type Route struct {
@@ -35,119 +28,277 @@ type Output struct {
 
 type MessageMap map[RouteID]Message
 
-type RxKernel func(MessageMap, MessageMap, chan struct{}) bool
+type InterruptFunc func() bool
+type KernelFunc func(MessageMap, MessageMap, chan InterruptFunc) InterruptFunc
 
-type Block struct {
-	Inputs  []Route
-	Outputs []Output
-	Kernel  RxKernel
-	Control chan interface{}
-	Quit    chan struct{}
+type Pin struct {
+	Name string
 }
 
-func NewBlock() *Block {
+type Spec struct {
+	Inputs  []Pin
+	Outputs []Pin
+	Kernel  KernelFunc
+}
+
+var Library = map[string]Spec{
+	"plus": Spec{
+		Inputs: []Pin{
+			Pin{
+				"addend",
+			},
+			Pin{
+				"addend",
+			},
+		},
+		Outputs: []Pin{
+			Pin{
+				"sum",
+			},
+		},
+		Kernel: func(in MessageMap, out MessageMap, i chan InterruptFunc) InterruptFunc {
+			out[0] = in[0].(float64) + in[1].(float64)
+			fmt.Println("Sending")
+			return nil
+		},
+	},
+	"delay": Spec{
+		Inputs: []Pin{
+			Pin{
+				"passthrough",
+			},
+			Pin{
+				"duration",
+			},
+		},
+		Outputs: []Pin{
+			Pin{
+				"passthrough",
+			},
+		},
+		Kernel: func(in MessageMap, out MessageMap, i chan InterruptFunc) InterruptFunc {
+			t, err := time.ParseDuration(in[1].(string))
+			if err != nil {
+				out[0] = err
+				return nil
+			}
+			timer := time.NewTimer(t)
+			select {
+			case <-timer.C:
+				out[0] = in[0]
+				return nil
+			case f := <-i:
+				return f
+			}
+			return nil
+		},
+	},
+	"log": Spec{
+		Inputs: []Pin{
+			Pin{
+				"passthrough",
+			},
+		},
+		Outputs: []Pin{
+			Pin{
+				"passthrough",
+			},
+		},
+		Kernel: func(in MessageMap, out MessageMap, i chan InterruptFunc) InterruptFunc {
+			fmt.Println(out[0])
+			out[0] = in[0]
+			return nil
+		},
+	},
+}
+
+type Block struct {
+	Inputs        []Route
+	Outputs       []Output
+	Kernel        KernelFunc
+	inputValues   MessageMap
+	outputValues  MessageMap
+	InterruptChan chan InterruptFunc
+	Quit          chan struct{}
+}
+
+func NewBlock(s Spec) *Block {
+	var in []Route
+	var out []Output
+
+	for _, v := range s.Inputs {
+
+		q, _ := fetch.Parse(".")
+		in = append(in, Route{
+			Name: v.Name,
+			Path: q,
+			C:    make(chan Message),
+		})
+	}
+
+	for _, v := range s.Outputs {
+		out = append(out, Output{
+			Name:        v.Name,
+			Connections: make(map[Connection]struct{}),
+		})
+	}
+
 	return &Block{
-		Inputs:  []Route{},
-		Outputs: []Output{},
-		Control: make(chan interface{}),
-		Quit:    make(chan struct{}),
+		Inputs:        in,
+		Outputs:       out,
+		Kernel:        s.Kernel,
+		inputValues:   make(MessageMap),
+		outputValues:  make(MessageMap),
+		InterruptChan: make(chan InterruptFunc),
 	}
 }
 
 func (b *Block) Serve() {
-	inputs := make(MessageMap)
-	outputs := make(MessageMap)
 	for {
-		if ok := b.Receive(inputs); !ok {
-			break
-		}
+		var interrupt InterruptFunc
+		for {
+			interrupt = b.Receive()
+			if interrupt != nil {
+				break
+			}
+			interrupt = b.Kernel(b.inputValues, b.outputValues, b.InterruptChan)
+			if interrupt != nil {
+				break
+			}
 
-		if ok = b.Kernel(inputs, outputs, b.QuitChan); !ok {
-			break
-		}
+			interrupt = b.Broadcast()
+			if interrupt != nil {
+				break
+			}
 
-		if ok = b.Broadcast(outputs); !ok {
-			break
+			// we've successfully completed one full loop
+			// empty input buffer
+			for k, _ := range b.inputValues {
+				delete(b.inputValues, k)
+			}
 		}
-
-		// we've successfully completed one full loop
-		// empty input buffer
-		for k, _ := range inputs {
-			delete(inputs, k)
+		fmt.Println("interrupt")
+		if ok := interrupt(); !ok {
+			return
 		}
 	}
 }
 
-func (b *Block) SetValue(id RouteId, value Message) {
-	b.Break <- struct{}{}
+func (b *Block) RouteValue(id RouteID, v Message) {
+	b.InterruptChan <- func() bool {
+		b.Inputs[id].Value = &v
+		return true
+	}
 }
 
-func (b *Block) SetPath(id RouteId, value *fetch.Query) {
-	b.Break <- struct{}{}
+func (b *Block) RoutePath(id RouteID, p *fetch.Query) {
+	b.InterruptChan <- func() bool {
+		b.Inputs[id].Path = p
+		b.Inputs[id].Value = nil
+		return true
+	}
 }
 
-func (b *Block) Connect(id RouteId, c Connection) {
-	b.Break <- struct{}{}
+func (b *Block) Connect(id RouteID, c Connection) {
+	b.InterruptChan <- func() bool {
+		b.Outputs[id].Connections[c] = struct{}{}
+		return true
+	}
 }
 
-func (b *Block) Disconnect(id RouteId, c Connection) {
-	b.Break <- struct{}{}
+func (b *Block) Disconnect(id RouteID, c Connection) {
+	b.InterruptChan <- func() bool {
+		delete(b.Outputs[id].Connections, c)
+		return true
+	}
 }
 
 func (b *Block) Stop() {
-	b.Break <- struct{}{}
+	b.InterruptChan <- func() bool {
+		return false
+	}
 }
 
-func (b *Block) Receive(values MessageMap) bool {
-	var err Error
+func (b *Block) Receive() InterruptFunc {
+	var err error
 	for id, input := range b.Inputs {
 		//if we have already received a value on this input, skip.
-		if _, ok := values[id]; ok {
+		if _, ok := b.inputValues[RouteID(id)]; ok {
 			continue
 		}
 
 		// if there is a value set for this input, place value on
 		// buffer and set it in map.
 		if input.Value != nil {
-			values[id] = *input.Value
+			b.inputValues[RouteID(id)] = *input.Value
 			continue
 		}
 
 		select {
 		case m := <-input.C:
-			v, err = fetch.Run(input.Path, m)
+			b.inputValues[RouteID(id)], err = fetch.Run(input.Path, m)
 			if err != nil {
 				log.Fatal(err)
 			}
-			values[id] = v
-		case <-b.QuitChan:
-			return nil, false
+		case f := <-b.InterruptChan:
+			return f
 		}
 	}
-	return values, true
+	return nil
 }
 
-func (b *Block) Broadcast(values MessageMap) bool {
-	for _, _ := range values {
-		/*o := b.GetOutput(k)
-		for c, _ := range o.GetConnections() {
+func (b *Block) Broadcast() InterruptFunc {
+	for id, out := range b.Outputs {
+		for c, _ := range out.Connections {
 			select {
-			case c <- v:
-			case <-b.QuitChan:
-				return false
+			case c <- b.outputValues[RouteID(id)]:
+			case f := <-b.InterruptChan:
+				return f
 			}
-		}*/
+		}
+
 	}
-	return true
+	return nil
 }
 
 func main() {
 	supervisor := suture.NewSimple("st-core")
 	supervisor.ServeBackground()
 
-	a := supervisor.Add(NewBlock())
+	b := NewBlock(Library["plus"])
+	d := NewBlock(Library["delay"])
+	l := NewBlock(Library["log"])
+
+	a := supervisor.Add(b)
+	_ = supervisor.Add(d)
+	_ = supervisor.Add(l)
+
+	b.Connect(0, d.Inputs[0].C)
+	d.Connect(0, l.Inputs[0].C)
+
+	b.RouteValue(RouteID(0), 1.1)
+	d.RouteValue(RouteID(1), "100ms")
+
+	go func() {
+		for {
+			b.RouteValue(RouteID(1), rand.Float64()*10.0)
+			time.Sleep(time.Duration(rand.Intn(200)+1) * time.Millisecond)
+		}
+
+	}()
 
 	time.Sleep(1 * time.Second)
+
+	fmt.Println("Disconnected!")
+
+	d.Disconnect(0, l.Inputs[0].C)
+
+	time.Sleep(100 * time.Millisecond)
+
+	d.Connect(0, l.Inputs[0].C)
+	fmt.Println("Connected")
+
+	time.Sleep(100 * time.Millisecond)
 
 	supervisor.Remove(a)
 

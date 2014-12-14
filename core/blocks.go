5236b1bc939a9cd566c3f286661939aa9f87a76d
Mike Dewar <mikedewar@gmail.com>
2014-12-14 01:43:12 +0000

    sent delay

--- core/blocks.go
+++ core/blocks.go
@@ -1,6 +1,8 @@
 package core
 
-// sum together numbers on each `addend`, returning result on `sum`
+import "time"
+
+// Sum sums together numbers on each addend
 func Plus() Spec {
 	return Spec{
 		Inputs: []Pin{
@@ -12,6 +14,41 @@ func Plus() Spec {
 		},
 		Kernel: func(in MessageMap, out MessageMap, i chan InterruptFunc) InterruptFunc {
 			out[0] = in[0].(float64) + in[1].(float64)
+			return nil
+		},
+	}
+}
+
+// Delay emits the message on passthrough after the specified duration
+func Delay() Spec {
+	return Spec{
+		Inputs: []Pin{
+			Pin{
+				"passthrough",
+			},
+			Pin{
+				"duration",
+			},
+		},
+		Outputs: []Pin{
+			Pin{
+				"passthrough",
+			},
+		},
+		Kernel: func(in MessageMap, out MessageMap, i chan InterruptFunc) InterruptFunc {
+			t, err := time.ParseDuration(in[1].(string))
+			if err != nil {
+				out[0] = err
+				return nil
+			}
+			timer := time.NewTimer(t)
+			select {
+			case <-timer.C:
+				out[0] = in[0]
+				return nil
+			case f := <-i:
+				return f
+			}
 			return nil
 		},
 	}

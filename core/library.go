e80fec9386172dc809d2b7d1632c85d723b48ee6
Mike Dewar <mikedewar@gmail.com>
2014-12-16 15:28:33 +0000

    moved Library back to being a top level variable

--- core/library.go
+++ core/library.go
@@ -6,77 +6,75 @@ import (
 	"time"
 )
 
-// return the streamtools library
-func GetLibrary() map[string]Spec {
-	return map[string]Spec{
-		"plus": Plus(),
-		"delay": Spec{
-			Inputs: []Pin{
-				Pin{
-					"passthrough",
-				},
-				Pin{
-					"duration",
-				},
+// Library is the set of all core block Specs
+var Library = map[string]Spec{
+	"plus": Plus(),
+	"delay": Spec{
+		Inputs: []Pin{
+			Pin{
+				"passthrough",
 			},
-			Outputs: []Pin{
-				Pin{
-					"passthrough",
-				},
-			},
-			Kernel: func(in MessageMap, out MessageMap, i chan Interrupt) Interrupt {
-				t, err := time.ParseDuration(in[1].(string))
-				if err != nil {
-					out[0] = err
-					return nil
-				}
-				timer := time.NewTimer(t)
-				select {
-				case <-timer.C:
-					out[0] = in[0]
-					return nil
-				case f := <-i:
-					return f
-				}
-				return nil
+			Pin{
+				"duration",
 			},
 		},
-		"set": Spec{
-			Inputs: []Pin{
-				Pin{
-					"key",
-				},
-				Pin{
-					"value",
-				},
-			},
-			Outputs: []Pin{
-				Pin{
-					"object",
-				},
-			},
-			Kernel: func(in MessageMap, out MessageMap, i chan Interrupt) Interrupt {
-				out[0] = map[string]interface{}{
-					in[0].(string): in[1],
-				}
-				return nil
+		Outputs: []Pin{
+			Pin{
+				"passthrough",
 			},
 		},
-		"log": Spec{
-			Inputs: []Pin{
-				Pin{
-					"log",
-				},
-			},
-			Outputs: []Pin{},
-			Kernel: func(in MessageMap, out MessageMap, i chan Interrupt) Interrupt {
-				o, err := json.Marshal(in[0])
-				if err != nil {
-					fmt.Println(err)
-				}
-				fmt.Println(string(o))
+		Kernel: func(in MessageMap, out MessageMap, i chan Interrupt) Interrupt {
+			t, err := time.ParseDuration(in[1].(string))
+			if err != nil {
+				out[0] = err
 				return nil
+			}
+			timer := time.NewTimer(t)
+			select {
+			case <-timer.C:
+				out[0] = in[0]
+				return nil
+			case f := <-i:
+				return f
+			}
+			return nil
+		},
+	},
+	"set": Spec{
+		Inputs: []Pin{
+			Pin{
+				"key",
+			},
+			Pin{
+				"value",
 			},
 		},
-	}
+		Outputs: []Pin{
+			Pin{
+				"object",
+			},
+		},
+		Kernel: func(in MessageMap, out MessageMap, i chan Interrupt) Interrupt {
+			out[0] = map[string]interface{}{
+				in[0].(string): in[1],
+			}
+			return nil
+		},
+	},
+	"log": Spec{
+		Inputs: []Pin{
+			Pin{
+				"log",
+			},
+		},
+		Outputs: []Pin{},
+		Kernel: func(in MessageMap, out MessageMap, i chan Interrupt) Interrupt {
+			o, err := json.Marshal(in[0])
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println(string(o))
+			return nil
+		},
+	},
 }

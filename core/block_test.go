6e9c3f57f8694a3659a896fe3b09dcf31c496429
nik <nikhan@gmail.com>
2014-12-25 20:44:38 -0500

    more tests

--- core/block_test.go
+++ core/block_test.go
@@ -90,3 +90,44 @@ func TestKeyValue(t *testing.T) {
 	}
 
 }
+
+func BenchmarkAddition(b *testing.B) {
+	sink := make(chan Message)
+	add := NewBlock(GetLibrary()["+"])
+	go add.Serve()
+	add.Connect(0, sink)
+	addend1 := add.Input(0).C
+	addend2 := add.Input(1).C
+
+	b.ResetTimer()
+	for i := 0; i < 100000; i++ {
+		addend1 <- 1.0
+		addend2 <- 2.0
+		_ = <-sink
+	}
+}
+
+func BenchmarkRandomMath(b *testing.B) {
+	sink := make(chan Message)
+	u1 := NewBlock(GetLibrary()["uniform"])
+	u2 := NewBlock(GetLibrary()["uniform"])
+	u3 := NewBlock(GetLibrary()["uniform"])
+	add := NewBlock(GetLibrary()["+"])
+	mul := NewBlock(GetLibrary()["×"])
+	go u1.Serve()
+	go u2.Serve()
+	go u3.Serve()
+	go add.Serve()
+	go mul.Serve()
+
+	u1.Connect(0, add.Input(0).C)
+	u2.Connect(0, add.Input(1).C)
+	add.Connect(0, mul.Input(0).C)
+	u3.Connect(0, mul.Input(1).C)
+	mul.Connect(0, sink)
+
+	b.ResetTimer()
+	for i := 0; i < 100000; i++ {
+		_ = <-sink
+	}
+}

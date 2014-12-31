73c8deba2085b2bb4f95cceb01e9ca8560a440f1
Mike Dewar <mikedewar@gmail.com>
2014-12-31 15:57:02 +0000

    muckin with docstrings

--- core/networkIO.go
+++ core/networkIO.go
@@ -11,8 +11,10 @@ import (
 
 // HTTPGET makes an HTTP GET request to the specified URL, emitting the response. If the response is JSON that can be unmarhsalled, then an interface containing the JSON is emitted. If not, then a string is emitted containing the response.
 //
-// * Pin 0: URL string
-// * Pin 1: header []string
+//Pin 0:
+//	URL string
+//Pin 1:
+//	header []string
 func HTTPGET() Spec {
 	return Spec{
 		Inputs:  []Pin{Pin{"URL"}, Pin{"header"}},

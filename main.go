7a03e182f231336743bc49501d6a97bc88f17f13
nik <nikhan@gmail.com>
2014-12-31 12:39:06 -0500

    naming consistency with /block

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	r.HandleFunc("/group", s.GetGroupHandler).Methods("GET")
 	r.HandleFunc("/group", s.CreateGroupHandler).Methods("POST")
 	r.HandleFunc("/block", s.CreateBlockHandler).Methods("POST")
-	r.HandleFunc("/connections", s.CreateConnectionHandler).Methods("POST")
+	r.HandleFunc("/connection", s.CreateConnectionHandler).Methods("POST")
 	http.Handle("/", r)
 
 	log.Println("serving on 7071")

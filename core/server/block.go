3c4ebb71f39d4e474c6c2b2242e1f329e8de1e6a
nik <nikhan@gmail.com>
2015-01-01 21:04:29 -0500

    adding block deletion

--- core/server/block.go
+++ core/server/block.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/nytlabs/st-core/core"
 	"github.com/thejerf/suture"
 )
@@ -90,7 +91,76 @@ func (s *Server) BlockCreate(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
 }
+
 func (s *Server) BlockDelete(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	ids, ok := vars["id"]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		writeJSON(w, Error{"no ID supplied"})
+		return
+	}
+
+	id, err := strconv.Atoi(ids)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		writeJSON(w, Error{err.Error()})
+		return
+	}
+
+	s.Lock()
+	defer s.Unlock()
+
+	b, ok := s.blocks[id]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		writeJSON(w, Error{"connection not found"})
+		return
+	}
+
+	deleteSet := make(map[int]struct{})
+
+	// build a set of connections that we may need to delete
+	// we need to panic here because if any error is thrown we are in huge trouble
+	// any panic indicates that our server connection ledger is no longer true
+	for _, c := range s.connections {
+		if c.Target.Id == id {
+			route, err := b.Block.GetRoute(core.RouteID(c.Target.Route))
+			if err != nil {
+				panic(err)
+			}
+			err = s.blocks[c.Source.Id].Block.Disconnect(core.RouteID(c.Source.Route), route.C)
+			if err != nil {
+				panic(err)
+			}
+			deleteSet[c.Id] = struct{}{}
+		}
+		if c.Source.Id == id {
+			route, err := s.blocks[c.Target.Id].Block.GetRoute(core.RouteID(c.Target.Route))
+			if err != nil {
+				panic(err)
+			}
+			err = b.Block.Disconnect(core.RouteID(c.Source.Route), route.C)
+			if err != nil {
+				panic(err)
+			}
+			deleteSet[c.Id] = struct{}{}
+		}
+	}
+
+	// delete the connections that involve this block
+	for k, _ := range deleteSet {
+		s.websocketBroadcast(Update{Action: DELETE, Type: CONNECTION, Data: s.connections[k]})
+		delete(s.connections, k)
+	}
+
+	// stop and delete the block
+	s.supervisor.Remove(b.Token)
+	s.websocketBroadcast(Update{Action: DELETE, Type: BLOCK, Data: s.blocks[id]})
+	delete(s.blocks, id)
+
+	w.WriteHeader(http.StatusNoContent)
+
 }
 func (s *Server) BlockModify(w http.ResponseWriter, r *http.Request) {
 }

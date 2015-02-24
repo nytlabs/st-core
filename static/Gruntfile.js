2ea9bb398885396119ea6327447a44582a12b9ca
Nik Hanselmann <nik@Media.local>
2015-02-24 00:22:32 -0500

    react

--- static/Gruntfile.js
+++ static/Gruntfile.js
@@ -1,37 +1,19 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'build/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
-            }
-        },
-        jst: {
-            compile: {
-                files: {
-                    "templates/build.js": ["templates/*.html"]
-                }
-            }
-        },
-        concat: {
-            dist: {
-                src: [
-                    'lib/*.js',
-                    'templates/*.js',
-                    'src/*.js'
-                ],
-                dest: 'build/<%= pkg.name %>.js',
+        react: {
+            jsx: {
+                files: [{
+                    expand: true,
+                    cwd: 'js',
+                    src: ['**/*.jsx'],
+                    dest: 'js',
+                    ext: '.js'
+                }]
             }
         },
     });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jst');
+    grunt.loadNpmTasks('grunt-react');
 
-    grunt.registerTask('default', ['jst', 'concat', 'uglify']);
+    grunt.registerTask('default', ['react']);
 };
---
+++ static/js/main.jsx
@@ -0,0 +1,16 @@
+var Stage = React.createClass({
+            getInitialState: function() {
+                return {
+                    entities: [],
+                    library: []
+                }
+            },
+            render: function() {
+                    return ( < div class = "stage" > < /div>
+	)
+    }
+})
+
+React.render( < h1 > Hello, world! < /h1 > ,
+                        document.getElementById('example')
+                    );

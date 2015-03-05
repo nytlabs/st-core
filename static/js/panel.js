065bcc218e5adee2fa0f8c9ff2ecb68a54189a9b
nik <nikhan@gmail.com>
2015-03-05 17:39:29 -0500

    minor panel

--- static/js/panel.js
+++ static/js/panel.js
@@ -6,11 +6,23 @@ var app = app || {};
         render: function() {
             return React.createElement('div', {
                 className: 'panel'
-            }, this.props.model.data.inputs.map(function(r, i) {
+            }, this.props.model.inputs.map(function(r, i) {
+                var status;
+                if (r.connections.length > 0) {
+                    status = "connected to " + r.connections.map(function(c) {
+                        return c.data.from.id
+                    }).join(",");
+                } else
+                if (r.data.value != null) {
+                    status = JSON.stringify(r.data.value);
+                } else {
+                    value = "not set";
+                }
+
                 return React.createElement('div', {
                     className: 'input',
                     key: i
-                }, r.name + ' ' + JSON.stringify(r.value))
+                }, r.data.name + ' ' + status)
 
             }));
         }

198cc54e8dd52374a51992106b5db61d64d56089
nik <nikhan@gmail.com>
2015-06-09 15:00:31 -0400

    tidy

--- static/js/component/block.js
+++ static/js/component/block.js
@@ -126,7 +126,6 @@ var app = app || {};
         render: function() {
             var classes = 'block';
             if (this.props.selected === true) classes += ' selected';
-            //if (this.props.model.data.alert === "blocked") classes += ' blocked';
             var children = [];
             children.push(React.createElement('rect', {
                 x: 0,

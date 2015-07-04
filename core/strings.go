7cbdf74d83a087ef82aaa34a65c4d40e942644d3
Michael Dewar <mikedewar@gmail.com>
2015-07-04 20:22:50 +0000

    actually adding the file

---
+++ core/strings.go
@@ -0,0 +1,40 @@
+package core
+
+import "strings"
+
+func StringConcat() Spec {
+	return Spec{
+		Name:    "concat",
+		Inputs:  []Pin{Pin{"a", STRING}, Pin{"b", STRING}},
+		Outputs: []Pin{Pin{"a+b", STRING}},
+		Kernel: func(in, out, internal MessageMap, s Source, i chan Interrupt) Interrupt {
+			a, ok := in[0].(string)
+			b, ok := in[1].(string)
+			if !ok {
+				out[0] = NewError("concat requires string")
+				return nil
+			}
+			out[1] = a + b
+			return nil
+		},
+	}
+}
+
+func StringSplit() Spec {
+	return Spec{
+		Name:    "split",
+		Inputs:  []Pin{Pin{"a<sep>b", STRING}, Pin{"sep", STRING}},
+		Outputs: []Pin{Pin{"[a,b]", ARRAY}},
+		Kernel: func(in, out, internal MessageMap, s Source, i chan Interrupt) Interrupt {
+			ab, ok := in[0].(string)
+			sep, ok := in[1].(string)
+			if !ok {
+				out[0] = NewError("split requires string")
+				return nil
+			}
+
+			out[0] = strings.Split(ab, sep)
+			return nil
+		},
+	}
+}

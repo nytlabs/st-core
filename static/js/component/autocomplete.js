380c101cc8da7e396b81d05bd53291fa8a05e24a
nik hanselmann <nikhan@gmail.com>
2015-11-19 11:36:02 -0500

    124

--- static/js/component/autocomplete.js
+++ static/js/component/autocomplete.js
@@ -75,8 +75,8 @@ var app = app || {};
                     'type': match.name,
                     'parent': app.NodeStore.getRoot(),
                     'position': {
-                        'x': this.props.x,
-                        'y': this.props.y
+                        'x': this.props.x + this.props.translateX,
+                        'y': this.props.y + this.props.translateY
                     }
                 },
                 null)
